cd55a50af08d19a9a896c1516067ec8731b88e5c
Quazi Fariha Fairooz <91182471+Qfariha@users.noreply.github.com>
2025-01-31 03:20:15 +0600

    changed

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 #from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm,ProfileUpdateForm
 from django.shortcuts import redirect
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
@@ -25,4 +25,22 @@ def register(request):
 
 @login_required #It is a decorator which adds funtionalities here it is working a condition to view profile
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method=='POST': #'POST' request=form submission
+        u_form=UserUpdateForm(request.POST,instance=request.user)
+        p_form=ProfileUpdateForm(request.POST,request.FILES,instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request,f'Your account has been updated!')
+            return redirect('profile')
+    
+    else: #If the request is not a form submission, the form fields are pre-filled with the user's existing data.
+        u_form=UserUpdateForm(instance=request.user)
+        p_form=ProfileUpdateForm(instance=request.user.profile)
+        
+    context={
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'users/profile.html',context)
